55a2cb14b254bcfef4a63ab6ffabf654ddda3891
saikrishna <saikrishna@saikrishnas-macbook-pro.local>
2016-02-03 15:41:30 +0530

    Initial Commit

---
+++ collectionview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionview
+//
+//  Created by saikrishna on 03/02/16.
+//  Copyright © 2016 saikrishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ collectionview/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  collectionview
+//
+//  Created by saikrishna on 03/02/16.
+//  Copyright © 2016 saikrishna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
